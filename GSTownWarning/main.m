0f3f543b25de8d6098f52042823ed6e3dd43c6f8
最爱海咪咪 <tachiever@126.com>
2017-09-28 09:12:36 +0800

    Initial Commit

---
+++ GSTownWarning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GSTownWarning
+//
+//  Created by Tcy on 2017/9/28.
+//  Copyright © 2017年 Tcytachiever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
